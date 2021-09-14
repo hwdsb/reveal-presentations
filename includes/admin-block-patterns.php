9c21cee16948fad96cbe2f01f4d346ab639f4c70
r-a-y <r-a-y@gmx.com>
2021-09-13 17:53:47 -0700

    Register 'layouts' block pattern category.
    Also adjust viewport width for first two block patterns.
    
    See #3.

--- includes/admin-block-patterns.php
+++ includes/admin-block-patterns.php
@@ -1,12 +1,17 @@
 <?php
-// Register only our patterns for slides.
+// Register block pattern category.
+register_block_pattern_category( 'layouts', array( 'label' => __( 'Layouts', 'reveal-presentations' ) ) );
+
+// Register block patterns.
 register_block_pattern(
 	'slides/heading-paragraph',
 	[
 		'title'       => __( 'Heading and Paragraph', 'reveal-presentations' ),
 		'blockTypes'  => [ 'core/heading' ],
+		'categories'  => [ 'layouts' ],
 		'description' => _x( 'A main header and a paragraph.', 'Block pattern description', 'reveal-presentations' ),
-		'content'     => '<!-- wp:heading {"level":1} --><h1></h1><!-- /wp:heading --><!-- wp:paragraph --><p></p><!-- /wp:paragraph -->'
+		'content'     => '<!-- wp:heading {"level":1} --><h1></h1><!-- /wp:heading --><!-- wp:paragraph --><p></p><!-- /wp:paragraph -->',
+		'viewportWidth' => '300',
 	]
 );
 
@@ -15,8 +20,10 @@ register_block_pattern(
 	[
 		'title'       => __( 'Heading and Text Columns', 'reveal-presentations' ),
 		'blockTypes'  => [ 'core/heading' ],
+		'categories'  => [ 'layouts' ],
 		'description' => _x( 'A main header plus two columns for your content.', 'Block pattern description', 'reveal-presentations' ),
-		'content'     => '<!-- wp:heading {"level":1} --><h1></h1><!-- /wp:heading --><!-- wp:columns --><div class="wp-block-columns"><!-- wp:column --><div class="wp-block-column"><!-- wp:paragraph --><p></p><!-- /wp:paragraph --></div><!-- /wp:column --><!-- wp:column --><div class="wp-block-column"><!-- wp:paragraph --><p></p><!-- /wp:paragraph --></div><!-- /wp:column --></div><!-- /wp:columns -->'
+		'content'     => '<!-- wp:heading {"level":1} --><h1></h1><!-- /wp:heading --><!-- wp:columns --><div class="wp-block-columns"><!-- wp:column --><div class="wp-block-column"><!-- wp:paragraph --><p></p><!-- /wp:paragraph --></div><!-- /wp:column --><!-- wp:column --><div class="wp-block-column"><!-- wp:paragraph --><p></p><!-- /wp:paragraph --></div><!-- /wp:column --></div><!-- /wp:columns -->',
+		'viewportWidth' => '300'
 	]
 );
 
@@ -25,6 +32,7 @@ register_block_pattern(
 	[
 		'title' => __( 'Text and Media', 'reveal-presentations' ),
 		'blockTypes'  => [ 'core/heading' ],
+		'categories'  => [ 'layouts' ],
 		'description' => _x( 'Display an image on the left with a heading on the right.', 'Block pattern description', 'reveal-presentations' ),
 		'content'     => '<!-- wp:media-text {"mediaType":"image","verticalAlignment":"top"} --><div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"><img src="https://hwdsbcommons.s3.amazonaws.com/wp-content/uploads/2019/09/CommonsIcon-300x300.png" /></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3,"className":"has-text-align-center"} --><h3 class="has-text-align-center"></h3><!-- /wp:heading --></div></div><!-- /wp:media-text -->'
 	]
