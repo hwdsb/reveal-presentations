964d82d47575de8049f5c744c191772e62184ebf
r-a-y <r-a-y@gmx.com>
2021-09-17 15:40:12 -0700

    Update image URL for 'Media and Text' block pattern.
    See #3.

--- includes/admin-block-patterns.php
+++ includes/admin-block-patterns.php
@@ -30,10 +30,10 @@ register_block_pattern(
 register_block_pattern(
 	'slides/text-media',
 	[
-		'title' => __( 'Text and Media', 'reveal-presentations' ),
+		'title'       => __( 'Text and Media', 'reveal-presentations' ),
 		'blockTypes'  => [ 'core/heading' ],
 		'categories'  => [ 'layouts' ],
 		'description' => _x( 'Display an image on the left with a heading on the right.', 'Block pattern description', 'reveal-presentations' ),
-		'content'     => '<!-- wp:media-text {"mediaType":"image","verticalAlignment":"top"} --><div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"><img src="https://hwdsbcommons.s3.amazonaws.com/wp-content/uploads/2019/09/CommonsIcon-300x300.png" /></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3,"className":"has-text-align-center"} --><h3 class="has-text-align-center"></h3><!-- /wp:heading --></div></div><!-- /wp:media-text -->'
+		'content'     => '<!-- wp:media-text {"mediaType":"image","verticalAlignment":"top"} --><div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"><img src="https://hwdsbcommons.s3.amazonaws.com/wp-content/uploads/sites/65/2021/09/media-text-replace.png" /></figure><div class="wp-block-media-text__content"><!-- wp:heading {"level":3,"className":"has-text-align-center"} --><h3 class="has-text-align-center"></h3><!-- /wp:heading --></div></div><!-- /wp:media-text -->'
 	]
 );
